d2f6cbab55544deba31e37fe4ec6649b22dbb683
Daniel Skowroński <daniel@skowron.ski>
2023-01-29 15:30:30 +0100

    rename files and add assets.csv manifest

--- build.sh
+++ build.sh
@@ -6,9 +6,10 @@ tar czf dsenv-dev.tgz dev/
 tar czf dsenv-mac.tgz mac/
 tar czf dsenv-zsh.tgz zsh/
 
-rm -f ASSETS.md
+rm -f ASSETS.md ASSETS.csv
 for asset in *.tgz; do
 	checksum=`sha256sum $asset | awk '{print $1}'`
 	link="https://github.com/danielskowronski/dsenv/releases/download/${GITHUB_REF_NAME}/${asset}"
 	echo "- [${link}](${link}) - \`${checksum}\`" >> ASSETS.md
+	echo "${asset},${link},${checksum}" >> assets.csv
 done
